92682a0cfad1db8e4a98e9a3c323696f94ed5b9a
Emanuel Escudero <escudero.mdz@gmail.com>
2023-10-09 12:03:34 -0300

    Logic

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,12 @@ import { Display } from './Display/Display';
 import './Main.css';
 import { useState } from 'react';
 import { evaluate } from 'mathjs';
-import { add } from '../../logic/logicMath';
+import {
+  add,
+  subtraction,
+  multiplication,
+  division,
+} from '../../logic/logicMath';
 
 export const Main = () => {
   /* Initial State of Display Input */
@@ -24,12 +29,67 @@ export const Main = () => {
     setResult(0);
   };
 
-  // Equal Fn
+  /* // Equal Fn
   const equalFn = () => {
     if (input) {
       const operation = evaluate(input);
       //setInput(evaluate(input));
       setResult(operation);
+    } else {
+      alert('Ingresa un valor');
+    }
+  }; */
+
+  // Importa o define todas las funciones de operaciones matemáticas aquí
+
+  // Función equalFn para realizar operaciones matemáticas
+  const equalFn = () => {
+    if (input) {
+      const parts = input.split(/([\+\-\*\/])/); // Divide la entrada en números y operadores
+      const numbers = [];
+      const operators = [];
+
+      // Separa los números y operadores
+      for (let part of parts) {
+        part = part.trim();
+        if (part === '+' || part === '-' || part === '*' || part === '/') {
+          operators.push(part);
+        } else if (part !== '') {
+          numbers.push(parseFloat(part));
+        }
+      }
+
+      if (numbers.length === 0 || operators.length === 0) {
+        alert('Entrada no válida');
+        return;
+      }
+
+      // Realiza las operaciones matemáticas
+      let result = numbers[0];
+
+      for (let i = 0; i < operators.length; i++) {
+        const operator = operators[i];
+        const nextNumber = numbers[i + 1];
+
+        if (operator === '+') {
+          result = add(result, nextNumber);
+        } else if (operator === '-') {
+          result = subtraction(result, nextNumber);
+        } else if (operator === '*') {
+          result = multiplication(result, nextNumber);
+        } else if (operator === '/') {
+          result = division(result, nextNumber);
+          if (
+            result === 'Invalid Operation' ||
+            result === 'You cannot calculate the division with zero'
+          ) {
+            alert(result);
+            return;
+          }
+        }
+      }
+
+      setResult(result);
     } else {
       alert('Ingresa un valor');
     }
--- src/logic/logicMath.js
+++ src/logic/logicMath.js
@@ -5,7 +5,7 @@ export function add(...numbers) {
 
 // Subtraction function
 export function subtraction(...numbers) {
-  if (numbers.length > 2) {
+  if (numbers.length >= 2) {
     return numbers.reduce((result, number) => result - number);
   } else {
     return numbers.reduce((result, number) => result - 0);
@@ -13,23 +13,23 @@ export function subtraction(...numbers) {
 }
 
 // Division function
-function division(...numbers) {
-  if (numbers.length > 2) {
+export function division(...numbers) {
+  if (numbers.length >= 2) {
     return numbers.reduce((result, number) => result / number);
   } else if (numbers.includes(0)) {
-    return 'Invalid Operation';
-  } else {
     return 'You cannot calculate the division with zero';
+  } else {
+    return 'Invalid Operation';
   }
 }
 
 // Multiplication function
-function multiplication(...numbers) {
+export function multiplication(...numbers) {
   return numbers.reduce((result, number) => result * number);
 }
 
 // Resto function
-function rest(...numbers) {
+export function rest(...numbers) {
   if (numbers.length < 2) {
     return 'Two numbers are required to calculate the remainder.';
   }
@@ -46,10 +46,65 @@ function percentage(number, percentage) {
 }
 
 // Change Sign function
-function changeSign() {
+export function changeSign() {
   let initialValue = parseFloat(display.value);
   if (!isNaN(initialValue)) {
     initialValue *= -1;
     display.value = newValue;
   }
 }
+
+// Importa o define todas las funciones de operaciones matemáticas aquí
+
+// Función equalFn para realizar operaciones matemáticas
+const equalFn = () => {
+  if (input) {
+    const parts = input.split(/([\+\-\*\/])/); // Divide la entrada en números y operadores
+    const numbers = [];
+    const operators = [];
+
+    // Separa los números y operadores
+    for (let part of parts) {
+      part = part.trim();
+      if (part === '+' || part === '-' || part === '*' || part === '/') {
+        operators.push(part);
+      } else if (part !== '') {
+        numbers.push(parseFloat(part));
+      }
+    }
+
+    if (numbers.length === 0 || operators.length === 0) {
+      alert('Entrada no válida');
+      return;
+    }
+
+    // Realiza las operaciones matemáticas
+    let result = numbers[0];
+
+    for (let i = 0; i < operators.length; i++) {
+      const operator = operators[i];
+      const nextNumber = numbers[i + 1];
+
+      if (operator === '+') {
+        result = add(result, nextNumber);
+      } else if (operator === '-') {
+        result = subtraction(result, nextNumber);
+      } else if (operator === '*') {
+        result = multiplication(result, nextNumber);
+      } else if (operator === '/') {
+        result = division(result, nextNumber);
+        if (
+          result === 'Invalid Operation' ||
+          result === 'You cannot calculate the division with zero'
+        ) {
+          alert(result);
+          return;
+        }
+      }
+    }
+
+    setResult(result);
+  } else {
+    alert('Ingresa un valor');
+  }
+};
